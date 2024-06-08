2fbc738b031981c8df67e9f8bab5592e29447124
HenryZhuHR <296506195@qq.com>
2024-06-08 19:32:13 +0800

    🛠️ aux: 添加解压缩权重脚本

---
+++ scripts/decompress-weights.sh
@@ -0,0 +1,46 @@
+
+cache_dir=.cache
+
+modelid_list=(
+    Qwen/Qwen2-0.5B
+    Qwen/Qwen2-0.5B-Instruct
+    Qwen/Qwen2-1.5B
+    Qwen/Qwen2-1.5B-Instruct
+    Qwen/Qwen2-7B
+    # Qwen/Qwen2-7B-Instruct
+)
+possible_compressed_file_suffixes=(
+    .tar.gz
+    .tgz
+    .tar.xz
+    .txz
+    .tar.bz2
+    .tbz2
+    .tar
+    .zip
+)
+
+for modelid in "${modelid_list[@]}"; do
+    if [ -d "$cache_dir/$modelid" ]; then
+        echo "Weights for $modelid already exist."
+    else
+        found_compressed_file=false
+        for suffix in "${possible_compressed_file_suffixes[@]}"; do
+            compressed_file="$cache_dir/$modelid$suffix"
+            if [ -f "$compressed_file" ]; then
+                echo "Decompressing $compressed_file..."
+                if [[ "$suffix" == ".zip" ]]; then
+                    unzip -o "$compressed_file" -d "$cache_dir"
+                else
+                    tar -xf "$compressed_file" -C "$cache_dir"
+                fi
+                echo "Decompressing weights for $modelid: $compressed_file"
+                found_compressed_file=true
+                break
+            fi
+        done
+        if [ "$found_compressed_file" = false ]; then
+            echo "Weights for $modelid not found."
+        fi
+    fi
+done
